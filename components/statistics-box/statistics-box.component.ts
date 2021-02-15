4754bc57b0a0f894ba4617fe10197d630508b8b6
kgisaackg <42145589+kgisaackg@users.noreply.github.com>
2021-02-15 02:56:31 +0200

    Add files via upload

---
+++ components/statistics-box/statistics-box.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-statistics-box',
+  templateUrl: './statistics-box.component.html',
+  styleUrls: ['./statistics-box.component.scss']
+})
+export class StatisticsBoxComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
